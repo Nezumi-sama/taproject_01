52467afcc0ab2cb8ac7750194fb0158c8f2ff687
Nezumi-sama <kryska007@yandex.ru>
2023-02-25 12:05:26 +0300

    Edit tests TestNG

--- src/test/java/ru/netology/service/CashBackHackerTest.java
+++ src/test/java/ru/netology/service/CashBackHackerTest.java
@@ -7,34 +7,34 @@ import static org.testng.Assert.*;
 public class CashBackHackerTest {
 
     @Test
-    public void remainAmount0() {
+    public void remainAmountRemainsNotMultiple1000() {
         CashBackHacker cashBackHacker = new CashBackHacker();
-        int amount = 0;
-
-        int actual = cashBackHacker.remain(amount);
-        int expected = 1000;
-
-        assertEquals(actual, expected);
-    }
-
-    @Test
-    public void remainAmountRemains0() {
-        CashBackHacker cashBackHacker = new CashBackHacker();
-        int amount = 2000;
-
-        int actual = cashBackHacker.remain(amount);
-        int expected = 1000;
-
-        assertEquals(actual, expected);
-    }
-
-    @Test
-    public void remainAmountRemainsNot0() {
-        CashBackHacker cashBackHacker = new CashBackHacker();
-        int amount = 1050;
+        int amount = 2050;
 
         int actual = cashBackHacker.remain(amount);
         int expected = 950;
+
+        assertEquals(actual, expected);
+    }
+
+
+    @Test
+    public void remainAmountRemains1000() {
+        CashBackHacker cashBackHacker = new CashBackHacker();
+        int amount = 1000;
+
+        int actual = cashBackHacker.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+    @Test
+    public void remainAmountRemainsMultiple1000() {
+        CashBackHacker cashBackHacker = new CashBackHacker();
+        int amount = 5000;
+
+        int actual = cashBackHacker.remain(amount);
+        int expected = 0;
 
         assertEquals(actual, expected);
     }
